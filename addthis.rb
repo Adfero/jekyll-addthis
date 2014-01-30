cf25a3a77e15c7bba397bdaea498818738060eac
John Jones <JJones@adfero.com>
2014-01-30 11:42:37 -0500

    Implemented easier-to-use syntax

--- addthis.rb
+++ addthis.rb
@@ -2,24 +2,35 @@ module AddThis
 	class ShareLink < Liquid::Tag
 		def initialize(name, params, tokens)
 			super
-			params = params.split(/\|/).map(&:strip)
-			@service = params[0]
-			@hash = params[1]
-			@title = params[2]
-			@teaser = params[3]
-			@image = params[4]
+			@service = params.delete(' ')
 		end
 
 		def render(context)
 			site_url = context.registers[:site].config['url']
-			pubid = context.registers[:site].config['addthis_pubid']
-			page_url = context.environments.first["page"]["url"]
-			image = "#{site_url}#{@image}"
-			url = "#{site_url}#{page_url}#{@hash}"
-			link = sprintf "http://api.addthis.com/oexchange/0.8/%s/offer?url=%s&title=%s&description=%s&pubid=%s&screenshot=%s", @service, CGI::escape(url), CGI::escape(@title), CGI::escape(@teaser), CGI::escape(pubid), CGI::escape(image)
-			sprintf "<a href=\"%s\" rel=\"nofollow\" class=\"share-link\" target=\"_blank\">%s</a>", link, @service
+			page_path = context.environments.first["page"]["url"]
+			image_path = context.registers[:site].config['addthis']['image']
+
+			url = "#{site_url}#{page_path}"
+			title = context.environments.first["page"]["title"]
+			teaser = context.environments.first["page"]["description"]
+			pubid = context.registers[:site].config['addthis']['pubid']
+			image = "#{site_url}#{image_path}"
+
+			ShareLink::share_url(@service,url,title,teaser,pubid,image)
+		end
+
+		def self.share_url(service, url, title, teaser, pubid, image)
+			servicename = service
+			if service != 'default'
+				service = 'forward/'+service+'/'
+			else
+				servicename = service
+				service = ''
+			end
+			link = sprintf "http://api.addthis.com/oexchange/0.8/%soffer?url=%s&title=%s&description=%s&pubid=%s&screenshot=%s", service, CGI::escape(url), CGI::escape(title), CGI::escape(teaser), CGI::escape(pubid), CGI::escape(image)
+			sprintf "<a href=\"%s\" rel=\"nofollow\" class=\"share-link share-%s\" target=\"_blank\">%s</a>", link, servicename, servicename
 		end
 	end
 end
 
-Liquid::Template.register_tag('share_link', AddThis::ShareLink)
+Liquid::Template.register_tag('sharelink', AddThis::ShareLink)
